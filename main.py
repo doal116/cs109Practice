99ee6384c250d1dbffdc3b6685ebf777a8b8d0a0
Alex Bulganin <bulganinbralex@yahoo.com>
2025-01-30 14:36:06 -0200

    feat: mle with gradient ascent added

--- main.py
+++ main.py
@@ -17,11 +17,16 @@ def main():
     # plt.plot(x, y, alpha=0.7, color="blue")
     # plt.show()
 
-    data_points = generate_data_point(20)
-    estimatedMean, estimatedVar, log_likelihood = mle.normal_without_tweak(
+    # data_points = generate_data_point(6)
+    data_points = [6.3, 5.5, 5.4, 7.1, 4.6, 6.7, 5.3, 4.8, 5.6,
+                   3.4, 5.4, 3.4, 4.8, 7.9, 4.6, 7.0, 2.9, 6.4, 6.0, 4.3]
+    estimatedMean, estimatedVar, log_likelihoodMle = mle.normal(
         data_points)
+    grad_asc_mean, grad_asc_var, log_likelihood = mle.normal_with_gradient_ascent(
+        data_points,0.1)
     print(data_points)
-    print(estimatedVar, estimatedMean, log_likelihood)
+    print(estimatedMean, estimatedVar, log_likelihoodMle)
+    print(grad_asc_mean, grad_asc_var, log_likelihood)
 
 
 if __name__ == "__main__":
--- mle.py
+++ mle.py
@@ -1,19 +1,48 @@
 import math
 
 
-def normal_without_tweak(data: list[float]) -> tuple[float, float, float]:
+def normal(data: list[float]) -> tuple[float, float, float]:
 
     estimated_mean: float = sum(data) / len(data)
 
-    estimated_var: float = 0.0
-    for x in data:
-        estimated_var += (x-estimated_mean)**2
-    estimated_var /= len(data)
+    estimated_var: float = sum(((x-estimated_mean)**2)
+                               for x in data) / (len(data) - 1)
 
     log_likelihood: float = 0.0
     for x in data:
         log_likelihood += (
-            -(math.log(math.sqrt(2*math.pi * estimated_var))) -
-            ((1/(2*estimated_var))*(x-estimated_mean)**2)
+            -(math.log(math.sqrt(2*math.pi) * estimated_var)) -
+            ((1/(2*estimated_var))*((x-estimated_mean)**2))
         )
     return estimated_mean, estimated_var, log_likelihood
+
+
+def normal_with_gradient_ascent(data: list[float], step: float) -> tuple[float, float, float]:
+    # Estimating mean through gradient ascent
+    grad_asc_mean: float = step * (sum(data) / len(data))
+    for x in data:
+        gradient = sum((x-grad_asc_mean)for x in data)
+        grad_asc_mean += step*(gradient/len(data))
+
+    # Estimating variance through gradient ascent
+    grad_asc_var: float = sum(
+        (x-grad_asc_mean)**2 for x in data) / (len(data)-1)
+
+    for _ in data:
+        gradient = (
+            -(len(data)/(2*grad_asc_var))
+            + (
+                (1/(2*(grad_asc_var**2)))
+                * (sum((x-grad_asc_mean)**2 for x in data))
+            )
+        )
+        grad_asc_var += (step * gradient)
+
+    # Log Likelihood of estimated parameters
+    log_likelihood: float = 0.0
+    for x in data:
+        log_likelihood += (
+            -(math.log(math.sqrt(2*math.pi) * grad_asc_var)) -
+            ((1/(2*grad_asc_mean))*(x-grad_asc_mean)**2)
+        )
+    return grad_asc_mean, grad_asc_var, log_likelihood
